4f8cd1fcc770996546dfbbca06e8c4e7ea011705
Đức Kiên <kluong960@gmail.com>
2025-10-07 07:21:47 +0700

    07/10

---
+++ week5/CorrectnessInvariant.java
@@ -0,0 +1,40 @@
+package edu.HW_DSA.week5;
+
+import java.util.Scanner;
+
+public class CorrectnessInvariant {
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        insertionSort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
---
+++ week5/CountingSort1.java
@@ -0,0 +1,35 @@
+package edu.HW_DSA.week5;
+
+import java.util.Scanner;
+
+public class CountingSort1 {
+
+    public static int[] countingSort(int[] arr) {
+        int[] frequencyArray = new int[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            frequencyArray[arr[i]]++;
+        }
+
+        return frequencyArray;
+    }
+
+    public static void main(String[] args) {
+        // Reading input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] result = countingSort(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+        sc.close();
+    }
+}
